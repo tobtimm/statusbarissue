32749a18f1402eddafb18ad813e69119f3b54690
Tobias Timm <tobias.timm@jayway.com>
2017-09-27 12:06:40 +0200

    Initial commit

--- Status bar test project/ViewController.swift
+++ Status bar test project/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleBarView: UIView!
+    
+    var statusBarHidden: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return self.statusBarHidden
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return UIStatusBarAnimation.fade
+    }
+    
+    @IBAction func tappedBackground(sender: UITapGestureRecognizer) {
+        if self.titleBarView.alpha == 0 {
+            fadeInViews()
+        } else {
+            fadeOutViews()
+        }
+    }
+ 
+    func fadeOutViews() {
+        UIView.animate(withDuration: 0.3) {
+            self.titleBarView.alpha = 0
+            self.statusBarHidden = true
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+    
+    func fadeInViews() {
+        UIView.animate(withDuration: 0.3) {
+            self.titleBarView.alpha = 1
+            self.statusBarHidden = false
+            self.setNeedsStatusBarAppearanceUpdate()
+        }
+    }
 }
 
