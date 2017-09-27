8c7b1543e6d20c5c05a2bc580a10c71e0011a06b
Tobias Timm <tobias.timm@jayway.com>
2017-09-27 10:05:12 +0200

    Initial Commit

---
+++ Status bar test project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Status bar test project
+//
+//  Created by Tobias Timm on 2017-09-27.
+//  Copyright © 2017 Tobias Timm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
